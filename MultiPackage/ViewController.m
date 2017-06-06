fc9f4b2612ea8f1f1cf549f9956238d04d9b15cd
nanhujiaju <hujiaju@hzflk.com>
2017-06-06 15:17:43 +0800

    Add PBKits pod lib

--- MultiPackage/ViewController.m
+++ MultiPackage/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <PBKits/PBKits.h>
+#define MAS_SHORT
+#define MAS_SHORT_HAND
+#import <Masonry/Masonry.h>
 
 @interface ViewController ()
 
@@ -19,16 +23,29 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     
-    CGRect bounds = CGRectMake(100, 100, 100, 50);
+    NSString *cnn = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"Channel"];
+    CGRect bounds = CGRectZero;
+    CGFloat offset = 100;
     UILabel *label = [[UILabel alloc] initWithFrame:bounds];
     label.backgroundColor = [UIColor lightGrayColor];
+    label.textAlignment = NSTextAlignmentCenter;
     label.textColor = [UIColor redColor];
+    label.text = cnn;
     [self.view addSubview:label];
+    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.equalTo(self.view).offset(offset);
+        make.right.equalTo(self.view).offset(-offset);
+        make.height.equalTo(@(offset * 0.5));
+    }];
     
-    bounds.origin.y += 100;
     UIImageView *imgv = [[UIImageView alloc] initWithFrame:bounds];
     imgv.contentMode = UIViewContentModeScaleAspectFill;
     [self.view addSubview:imgv];
+    [imgv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(label.mas_bottom).offset(offset);
+        make.left.right.equalTo(label);
+        make.height.equalTo(@(offset*0.5));
+    }];
     
 }
 
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, 8.0
+
+target 'MultiPackage' do
+pod 'PBKits'
+pod 'Masonry'
+end
